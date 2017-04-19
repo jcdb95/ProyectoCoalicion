2f17e765dc82f90fd8100b8ca32e45b16069a620
tomas <tomasmartty@gmail.com>
2017-04-19 15:15:22 -0300

    ADD: Splashscreen, Colors & Styles

---
+++ app/src/main/java/coalicion/proyectocoalicion/SplashScreen.java
@@ -0,0 +1,38 @@
+package coalicion.proyectocoalicion;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by tomas on 19/04/17.
+ */
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
