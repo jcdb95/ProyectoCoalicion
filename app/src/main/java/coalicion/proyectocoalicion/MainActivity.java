262831c68c16abf516186cf1bd32a94a122b5f72
tomas <tomasmartty@gmail.com>
2017-04-19 16:06:34 -0300

    FIX: OnClickListeners en vez de Intents para abrir carreras

--- app/src/main/java/coalicion/proyectocoalicion/MainActivity.java
+++ app/src/main/java/coalicion/proyectocoalicion/MainActivity.java
@@ -4,44 +4,75 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Arquitectura OnClickListener
+        TextView arquitectura = (TextView) findViewById(R.id.carrera_arquitectura);
+        arquitectura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent arquitecturaIntent = new Intent(MainActivity.this, Arquitectura.class);
+                startActivity(arquitecturaIntent);
+            }
+        });
+
+        // Diseno Grafico OnClickListener
+        TextView diseno_grafico = (TextView) findViewById(R.id.carrera_disenografico);
+        diseno_grafico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent disenograficoIntent = new Intent(MainActivity.this, DG.class);
+                startActivity(disenograficoIntent);
+            }
+        });
+
+        // Imagen y Sonido OnClickListener
+        TextView imagenysonido = (TextView) findViewById(R.id.carrera_imagenysonido);
+        imagenysonido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imagenysonidoIntent = new Intent(MainActivity.this, DIS.class);
+                startActivity(imagenysonidoIntent);
+            }
+        });
+
+        // Diseno de Indumentaria OnClickListener
+        TextView disenoindumentaria = (TextView) findViewById(R.id.carrera_disenoindumentaria);
+        disenoindumentaria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent disenoindumentariaIntent = new Intent(MainActivity.this, DIT.class);
+                startActivity(disenoindumentariaIntent);
+            }
+        });
+
+        // Diseno Industrial OnClickListener
+        TextView disenoindustrial = (TextView) findViewById(R.id.carrera_disenoindustrial);
+        disenoindustrial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent disenoindustrialIntent = new Intent(MainActivity.this, DI.class);
+                startActivity(disenoindustrialIntent);
+            }
+        });
+
+        // Paisajismo OnClickListener
+        TextView paisajismo = (TextView) findViewById(R.id.carrera_paisajismo);
+        paisajismo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent paisajismoIntent = new Intent(MainActivity.this, DPAIS.class);
+                startActivity(paisajismoIntent);
+            }
+        });
+
+
     }
-
-    public void abrirArquitectura (View view) {
-        startActivity(new Intent(this, Arquitectura.class));
-
-    }
-
-    public void abrirDisenoGrafico (View view) {
-        startActivity(new Intent(this, DG.class));
-
-    }
-
-    public void abrirDisenoImgYSonido (View view) {
-        startActivity(new Intent(this, DIS.class));
-
-    }
-
-    public void abrirDisenoIndYText (View view) {
-        startActivity(new Intent(this, DIT.class));
-
-    }
-
-    public void abrirDisenoIndustrial (View view) {
-        startActivity(new Intent(this, DI.class));
-
-    }
-
-    public void abrirPaisajismo (View view) {
-        startActivity(new Intent(this, DPAIS.class));
-
-    }
-
-
-
 }
