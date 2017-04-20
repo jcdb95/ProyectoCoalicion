a83b1483a30c350179a215993eb97e003a1090ae
tomas <tomasmartty@gmail.com>
2017-04-20 16:07:24 -0300

    Generar materias dinamicamente (parcial)

--- app/src/main/java/coalicion/proyectocoalicion/Arquitectura.java
+++ app/src/main/java/coalicion/proyectocoalicion/Arquitectura.java
@@ -1,18 +1,71 @@
 package coalicion.proyectocoalicion;
 
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class Arquitectura extends CarrerasActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_arquitectura);
+        setContentView(R.layout.);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         myToolbar.showOverflowMenu();
         reloadAll();
+
+
+        setContentView(R.layout.materias_list);
+
+        final ArrayList<MateriaClass> materias = new ArrayList<MateriaClass>();
+        materias.add(new MateriaClass("Sociedad y Estado"));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+        materias.add(new MateriaClass(""));
+
+
+
+
+
+        MateriaClassAdapter adapter = new MateriaClassAdapter(this, materias);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
     }
 
 
